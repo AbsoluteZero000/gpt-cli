d619babec3add76edc2b01572129e529d7da1890
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-09-24 22:13:18 +0300

    feat: added user input

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,30 @@ use reqwest::blocking::Client;
 use serde_json::json;
 use std::env;
 use std::error::Error;
+use std::io::{stdin, stdout, Write};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Load environment variables from .env file
     dotenv().ok();
 
-    // Set up the API URL and the API key from environment variable
+    let mut input = String::new();
+    print!("> ");
+
+    let _ = stdout().flush();
+    stdin().read_line(&mut input).expect("Failed to read line");
+    println!();
+
     let api_key = env::var("API_KEY").expect("API_KEY must be set");
     let url = format!(
         "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={}",
         api_key
     );
 
-    // Prepare the JSON body for the request
     let json_body = json!({
         "contents": [
             {
                 "parts": [
                     {
-                        "text": "Explain how AI works"
+                        "text": input
                     }
                 ]
             }
@@ -35,7 +40,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         .json(&json_body)
         .send()?;
 
-    println!("Response: {:?}", resp.text()?);
+    let data = resp.json::<serde_json::Value>()?;
+    let resp = data["candidates"][0]["content"]["parts"][0]["text"]
+        .as_str()
+        .unwrap_or("");
+
+    println!("GEMINI:\n {}", resp);
 
     Ok(())
 }
