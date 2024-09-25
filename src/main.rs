61ae8ca47ef3d69151a4b346e16fd21c4b77af72
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-09-25 19:27:51 +0300

    feat: added history

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 use dotenvy::dotenv;
 use reqwest::blocking::Client;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
+use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 use std::io::{stdin, stdout, Write};
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Part {
+    text: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Message {
+    role: String,
+    parts: Vec<Part>,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
+
+    let mut conversations: HashMap<String, Vec<Message>> = HashMap::new();
+
+    println!("GPT CLI");
 
     loop {
         let mut input = String::new();
@@ -18,7 +35,31 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         if input.trim() == "exit" {
             break;
+        } else if input.trim() == "switch" || input.trim() == "clear" || input.trim() == "new" {
+            // TODO: split all of them and give each one there functionality
+            conversations = HashMap::new();
+            continue;
+        } else if input.trim() == "help" {
+            println!("Commands:");
+            println!("new    - Start a new conversation");
+            println!("switch - Switch to another conversation");
+            println!("clear  - Clear the conversation history");
+            println!("exit   - Exit the program\n");
+            continue;
         }
+
+        let message = Message {
+            role: "user".to_string(),
+            parts: vec![Part {
+                text: input.trim().to_string(),
+            }],
+        };
+
+        let entry = conversations
+            .entry("conversation_1".to_string())
+            .or_insert(vec![]);
+
+        entry.push(message);
 
         let api_key = env::var("API_KEY").expect("API_KEY must be set");
         let url = format!(
@@ -26,16 +67,18 @@ fn main() -> Result<(), Box<dyn Error>> {
             api_key
         );
 
+        let contents: Vec<_> = conversations["conversation_1"]
+            .iter()
+            .map(|message| {
+                json!({
+                    "role": message.role,
+                    "parts": message.parts.iter().map(|part| json!({"text": part.text})).collect::<Vec<_>>()
+                })
+            })
+            .collect();
+
         let json_body = json!({
-            "contents": [
-                {
-                    "parts": [
-                        {
-                            "text": input.trim()
-                        }
-                    ]
-                }
-            ]
+            "contents": contents
         });
 
         let client = Client::new();
